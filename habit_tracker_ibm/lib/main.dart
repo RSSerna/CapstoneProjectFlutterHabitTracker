eb12a383a24ce978ca4edaf86a54b52bb7732c3b
RicardoSaldarriagaS <=>
2025-11-08 17:40:30 -0500

    Fix Flutter App, add Login and Register UI

--- habit_tracker_ibm/lib/main.dart
+++ habit_tracker_ibm/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker_ibm/screens/login_screen.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() {
+  runApp(HabitTrackerApp());
+}
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+class HabitTrackerApp extends StatelessWidget {
+  const HabitTrackerApp({super.key});
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
